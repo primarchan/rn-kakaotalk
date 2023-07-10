fb01c2c0fbabff2a1280f7199cd7a54b7fc517c9
An T.G <boltcarrier@naver.com>
2023-07-10 14:12:14 +0900

    친구 목록 UI ScrollView -> FlatList로 리팩토링

--- App.js
+++ App.js
@@ -13,6 +13,7 @@ import FriendSection from "./src/FriendSection";
 import FriendList from "./src/FriendList";
 import { useState } from "react";
 import TabBar from "./src/TabBar";
+import { FlatList } from "react-native";
 
 const statusBarHeight = getStatusBarHeight(true);
 const bottomSpace = getBottomSpace();
@@ -26,6 +27,70 @@ export default function App() {
   const onPressArrow = () => {
     setIsOpened(!isOpened);
   };
+
+  const ItemSeparatorComponent = () => <Margin height={13} />;
+
+  const ListHeaderComponent = () => (
+    <View style={{ flex: 1, paddingHorizontal: 15, backgroundColor: "white" }}>
+      <Header />
+
+      <Margin height={10} />
+
+      <Profile
+        uri={myProfile.uri}
+        name={myProfile.name}
+        introduction={myProfile.introduction}
+        isMe={true}
+      />
+
+      <Margin height={15} />
+
+      <Division />
+
+      <Margin height={12} />
+
+      <FriendSection
+        friendProfileLen={friendProfiles.length}
+        onPressArrow={onPressArrow}
+        isOpened={isOpened}
+      />
+
+      <Margin height={5} />
+    </View>
+  );
+
+  const renderItem = ({ item }) => (
+    <View>
+      <Profile
+        uri={item.uri}
+        name={item.name}
+        introduction={item.introduction}
+        isMe={false}
+      />
+    </View>
+  );
+
+  const ListFooterComponent = () => <Margin height={10} />;
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={isOpened ? friendProfiles : []}
+        contentContainerStyle={{ paddingHorizontal: 15 }}
+        stickyHeaderIndices={[0]}
+        keyExtractor={(_, index) => index}
+        ItemSeparatorComponent={ItemSeparatorComponent}
+        renderItem={renderItem}
+        ListHeaderComponent={ListHeaderComponent}
+        ListFooterComponent={ListFooterComponent}
+        showsVerticalScrollIndicator={false}
+      />
+      <TabBar
+        selectedTabIdx={selectedTabIdx}
+        setSelectedTabIdx={setSelectedTabIdx}
+      />
+    </View>
+  );
 
   return (
     <View style={styles.container}>
--- src/Profile.js
+++ src/Profile.js
@@ -2,18 +2,31 @@ import { Image, Text, View } from "react-native";
 import Margin from "./Margin";
 
 export default (props) => {
+  const size = props.isMe ? 50 : 40;
+
   return (
     <View style={{ flexDirection: "row" }}>
       <Image
         source={{ uri: props.uri }}
-        style={{ width: 50, height: 50, borderRadius: 20 }}
+        style={{ width: size, height: size, borderRadius: size * 0.4 }}
       />
       <View style={{ justifyContent: "center", marginLeft: 10 }}>
-        <Text style={{ fontWeight: "bold", fontSize: 16 }}>{props.name}</Text>
-        <Margin height={6} />
-        <Text style={{ fontSize: 12, color: "grey" }}>
-          {props.introduction}
+        <Text
+          style={{
+            fontWeight: props.isMe ? "bold" : undefined,
+            fontSize: props.isMe ? 16 : 15,
+          }}
+        >
+          {props.name}
         </Text>
+        {!!props.introduction && (
+          <View>
+            <Margin height={props.isMe ? 6 : 2} />
+            <Text style={{ fontSize: props.isMe ? 12 : 11, color: "grey" }}>
+              {props.introduction}
+            </Text>
+          </View>
+        )}
       </View>
     </View>
   );
